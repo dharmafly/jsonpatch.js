3b1b2337c0162e0e484155f962381de8f597c1f5
Benny Neugebauer <mail@bennycode.com>
2024-07-18 14:50:44 +0200

    Strictly type operations

--- lib/jsonpatch.d.ts
+++ lib/jsonpatch.d.ts
@@ -1,4 +1,41 @@
-export function apply_patch<D>(doc: D, patch: string | Array<any>): D;
+export interface AddOperation {
+  op: 'add';
+  path: string;
+  value: any;
+}
+
+export interface RemoveOperation {
+  op: 'remove';
+  path: string;
+}
+
+export interface ReplaceOperation {
+  op: 'replace';
+  path: string;
+  value: any;
+}
+
+export interface MoveOperation {
+  op: 'move';
+  path: string;
+  from: string;
+}
+
+export interface CopyOperation {
+  op: 'copy';
+  path: string;
+  from: string;
+}
+
+export interface TestOperation {
+  op: 'test';
+  path: string;
+  value: any;
+}
+
+export type Operation = AddOperation | RemoveOperation | ReplaceOperation | MoveOperation | CopyOperation | TestOperation;
+
+export function apply_patch<D>(doc: D, patch: string | Array<Operation>): D;
 
 export interface InvalidPatch extends Error {
   new (message: string): InvalidPatch;
@@ -18,6 +55,6 @@ export interface JSONPointer {
 }
 
 export interface JSONPatch {
-  new (patch: string | Array<any>, mutate?: boolean): JSONPatch;
+  new (patch: string | Array<Operation>, mutate?: boolean): JSONPatch;
   apply<D>(doc: D): D;
 }
